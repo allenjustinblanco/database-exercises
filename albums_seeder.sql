f4853ad25de09d24c5ee284851fbc5dd4399fad6
allenjustinblanco <blanco.allenjustin@gmail.com>
2019-01-28 15:44:06 -0600

    Added albums, and used select to write queries

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,2 +1,8 @@
 use codeup_test_db;
-insert into 
+
+insert into albums (artists, name, release_date, sales, genre) values ("Michael Jackson", "Thriller", 1976, 66),
+("Eagles", "Thei Greates Hits", 1976, 51),
+("AC/DC", "Back in Black", 1980, 50),
+("Pink Floyd", "The Dark Side Of The Moon", 1973, 45),
+("Whitney Houston", "The Bodyguard", 1992, 45),
+("Meat Loaf", "Bat Out of Hell", 1977, 43);
--- select_exercises.sql
+++ select_exercises.sql
@@ -1,4 +1,7 @@
 use codeup_test_db;
 select * from albums where name = 'Pink Floyd';
 select * from albums where genre = 'Nevermind';
-select * from albums where 
+select * from albums where release_date like '%199%';
+select * from albums where release_date = '1967';
+select * from albums where sales < 20;
+select * from albums where genre = 'Rock';
