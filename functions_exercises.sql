ac249e2a9e6acedca55df10b19a040c58a961420
allenjustinblanco <blanco.allenjustin@gmail.com>
2019-01-29 17:06:51 -0600

    finished functions mysql exercise

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -45,13 +45,10 @@ SELECT * FROM employees WHERE hire_date > '199%'
                           AND birth_date LIKE '%12-25'
 ORDER BY birth_date, hire_date DESC;
 
-SELECT *
+select concat('Days Working at the company', datediff(now(), hire_date))
 FROM employees
-WHERE hire_date > '199%';
-
-SELECT *
-FROM employees
-WHERE hire_date LIKE '%12-25';
+WHERE hire_date > '199%'
+and hire_date LIKE '%12-25';
 
 SELECT *
 FROM employees
@@ -59,3 +56,7 @@ WHERE last_name LIKE '%q%';
 
 SELECT * FROM employees WHERE last_name NOT LIKE '%qu%' AND last_name LIKE '%q%';
 
+select concat(first_name, ' ', last_name)
+from employees
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%E';
