da527830e04df3aa49b85dd49de2b0028ddd697c
allenjustinblanco <blanco.allenjustin@gmail.com>
2019-01-29 14:11:29 -0600

    Finished work for Order by mysql exercise

---
+++ order_by_exercises.sql
@@ -0,0 +1,61 @@
+use employees;
+
+# order by first name
+SELECT * FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+  AND gender = 'M'
+ORDER BY first_name;
+
+# order by first then last name with first result as Irena and last as Vidya
+SELECT * FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+  AND gender = 'M'
+ORDER BY first_name, last_name;
+
+# order by last name before first name
+SELECT * FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+  AND gender = 'M'
+ORDER BY last_name, first_name;
+
+# sort employees with E by employee number
+SELECT * FROM employees
+WHERE last_name LIKE 'E%'
+   OR last_name LIKE '%E'
+ORDER BY emp_no;
+
+SELECT * FROM employees
+WHERE last_name LIKE 'E%'
+  AND last_name LIKE '%E'
+ORDER BY emp_no;
+
+
+# reverse the sort order for both queries
+SELECT * FROM employees
+WHERE last_name LIKE 'E%'
+   OR last_name LIKE '%E'
+ORDER BY emp_no DESC;
+
+SELECT * FROM employees
+WHERE last_name LIKE 'E%'
+  AND last_name LIKE '%E'
+ORDER BY emp_no DESC;
+
+SELECT * FROM employees WHERE hire_date > '199%'
+                          AND birth_date LIKE '%12-25'
+ORDER BY birth_date, hire_date DESC;
+
+SELECT *
+FROM employees
+WHERE hire_date > '199%';
+
+SELECT *
+FROM employees
+WHERE hire_date LIKE '%12-25';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%';
+
+SELECT * FROM employees WHERE last_name NOT LIKE '%qu%' AND last_name LIKE '%q%';
+
--- where_exercises.sql
+++ where_exercises.sql
@@ -10,11 +10,11 @@ where last_name like 'E%';
 
 select *
 from employees
-where hire_date < 1990;
+where hire_date like '199%';
 
 select *
 from employees
-where birth_date = '12-25-%';
+where birth_date = '%-12-25';
 
 select *
 from employees
@@ -26,11 +26,19 @@ where last_name like '%E';
 
 select *
 from employees
-where hire_date between 1990 and 2000;
+where hire_date between '1990-01-01' and '1999-12-31';
 
 select *
 from employees
 where last_name like '%q%'
 and last_name not like '%qu%';
+
+SELECT * FROM employees
+WHERE last_name LIKE 'e%'
+   OR last_name LIKE '%e';
+
+SELECT * FROM employees
+WHERE last_name LIKE 'E%'
+  AND last_name LIKE '%E';
 
 
