d6b9f5da884e58186061617fe2a7ce25a59a4200
allenjustinblanco <blanco.allenjustin@gmail.com>
2019-01-29 11:37:12 -0600

    finished work for where sql exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -2,9 +2,7 @@ use employees;
 
 select *
 from employees
-where first_name = 'Irena'
-or first_name = 'Vidya'
-or first_name = 'Maya';
+where first_name in ('Irena', 'Vidya', 'Maya');
 
 select *
 from employees
@@ -21,5 +19,18 @@ where birth_date = '12-25-%';
 select *
 from employees
 where last_name like '%q%';
+
+select *
+from employees
+where last_name like '%E';
+
+select *
+from employees
+where hire_date between 1990 and 2000;
+
+select *
+from employees
+where last_name like '%q%'
+and last_name not like '%qu%';
 
 
